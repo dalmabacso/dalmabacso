92ea06e07dcb92afc0cfef351cbae173745a9dc1
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2021-10-12 09:34:07 +0300

    Add files via upload

---
+++ paros sor,paros oszlop osszege/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int v[100][100],i,j,S1=0,S2=0,n;
+   cout <<"n=";
+   cin >> n;
+   for(i=0:i<n;i++){
+     for(j=0;j<n;j++){
+        cin >>v[i][j];
+     }
+   }
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+          if(j%2==0%%2==0){
+            S1=v[i][j];
+          }
+    }
+    }
+    cout <<S;
+
+    return 0;
+}
