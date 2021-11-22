bae7f0d128a05c159fb4703f1b831ddb9b2d388c
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2021-11-22 18:26:18 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,54 @@
 #include <iostream>
-#include<algorithm>
-
+#include <fstream>
 using namespace std;
 
 int main()
 {
-    string str;
-    cout <<"Kerem a szoveget";
-    cin >>str;
-    int n=str.size();
-    swap (str[0],str[n-1]);
-    for (int i=1;i<n-1;i++){
-        swap (str[i],str[n-1]);
-      str[i]=str [i-1];
+    int v[100][100], n, SK=0, SNY=0;
+    int lnko = 0;
+    int c, d;
+    ifstream in ("input.txt");
+    ofstream out ("lnko.out");
+    in >> n;
+     for (int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+           in >> v[i][j];
+        }
+     }
+    for (int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if (i<j && i+j>n-1){
+                SK= SK + v[i][j];
+            }
+             if (i>j && i+j<n-1){
+                SNY= SNY + v[i][j];
+            }
+        }
     }
-     cout <<str;
-    return 0;
 
+
+c = SK;
+d = SNY;
+
+if( SK < SNY )
+{
+    int p = SK;
+    SK = SNY;
+    SNY = p;
+}
+
+while( SNY != 0 )
+{
+    int m = SK % SNY;
+    SK = SNY;
+     SNY = m;
+}
+
+lnko = SK;
+
+
+   out << "lnko=" << lnko << endl;
+   in.close();
+   out.close();
+    return 0;
 }
