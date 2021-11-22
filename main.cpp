d8723f6ec66966b82c6a763b49ab7467350480e5
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2021-11-22 18:48:51 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,47 +1,54 @@
 #include <iostream>
 #include <fstream>
-
 using namespace std;
 
 int main()
 {
-    ifstream in("input.txt");
-    ofstream out ("output.txt");
-    int v[100][100],n,i,j,f=0,a=0;
-    in >>n;
-    for (int i=0;i<n;i++){
-        for (int j=0;j<n;j++){
-            in >>v[i][j];
-
+    int v[100][100], n, SK=0, SNY=0;
+    int lnko = 0;
+    int c, d;
+    ifstream in ("input.txt");
+    ofstream out ("lnko.out");
+    in >> n;
+     for (int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+           in >> v[i][j];
+        }
+     }
+    for (int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if (i<j && i+j>n-1){
+                SK= SK + v[i][j];
+            }
+             if (i>j && i+j<n-1){
+                SNY= SNY + v[i][j];
+            }
         }
     }
-    for (int i=0;i<n;i++){
-        for (int j=0;j<n;j++){
-                if(i<j){
-                    f+=v[i][j];
-
-                }
-                if(i<j){
-                    a+=v[i][j];
-
-                }
-                int x,lkkt=0;
-                x=f*a;
-                while (a!=f){
-                    if(f>a){
-                        f=f-a;
-
-                    }else{
-                    a=a-f;}
-                    }
-                    lkkt=x/a;
-                    cout <<"lkkt"<<lkkt;
-                    in.close();
-                    out.close();
-                    }
-
-                }
 
 
+c = SK;
+d = SNY;
+
+if( SK < SNY )
+{
+    int p = SK;
+    SK = SNY;
+    SNY = p;
+}
+
+while( SNY != 0 )
+{
+    int m = SK % SNY;
+    SK = SNY;
+     SNY = m;
+}
+
+lnko = SK;
+
+
+   out << "lnko=" << lnko << endl;
+   in.close();
+   out.close();
     return 0;
 }
