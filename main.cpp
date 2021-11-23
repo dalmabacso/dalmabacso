9090f9fb2f79d8050324ed7816ae55bcdb784cc4
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2021-11-23 09:31:08 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,54 +1,23 @@
 #include <iostream>
-#include <fstream>
+
 using namespace std;
-
-int main()
-{
-    int v[100][100], n, SK=0, SNY=0;
-    int lnko = 0;
-    int c, d;
-    ifstream in ("input.txt");
-    ofstream out ("lnko.out");
-    in >> n;
-     for (int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-           in >> v[i][j];
-        }
-     }
-    for (int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if (i<j && i+j>n-1){
-                SK= SK + v[i][j];
-            }
-             if (i>j && i+j<n-1){
-                SNY= SNY + v[i][j];
-            }
-        }
-    }
-
-
-c = SK;
-d = SNY;
-
-if( SK < SNY )
-{
-    int p = SK;
-    SK = SNY;
-    SNY = p;
+bool prim (int a){
+for (int oszto =2;oszto<a/2+1;oszto++){
+  if (a%oszto ==0){
+    return false;
+  }
 }
+  return true;
 
-while( SNY != 0 )
+  }
+int main ()
 {
-    int m = SK % SNY;
-    SK = SNY;
-     SNY = m;
+  if (prim (9)){
+    cout <<"A szam prim";
+
+  }else{
+  cout << "A szam nem prim";
+
 }
-
-lnko = SK;
-
-
-   out << "lnko=" << lnko << endl;
-   in.close();
-   out.close();
     return 0;
 }
