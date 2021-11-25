07bd4d6a0ca2da270cb4d20a3fae0492f7ddd6b6
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2021-11-25 09:32:04 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 
 using namespace std;
+int divPrim (int n){
+int oszto=2;
+int S=0;
+while (n>1){
+  int szamlalo =0;
+  while (n%oszto==0){
+    n/=oszto;
+    szamlalo++;
 
-int lnko(int a,int b){
- while (a!=b){
-  if(a>b){
-    a=a-b;
-
-  }else{
-  b=b-a;
-  return o;
   }
- int main (){
- int a,b;
- cout << "a=";
- cin >>a;
- cout <<"b=";
- cin >>b;
- cout << lnko (a,b);
+  if (szamlalo%2>0)
+{
+  S+=oszto;
 
-    return 0;
+  }
+  oszto++;
+}
+return S;
+}
+
+int main()
+{
+   int s=divPrim(360);
+   cout << s<<endl;
+   return 0;
 }
