7dde7e9547c41c381d0cb4e3866b0228c8cdecfd
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2022-01-06 09:29:17 +0200

    Add files via upload

---
+++ labor 14-2 fel/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int hatvany (int x,int n){
+if (n==0){
+  return 1;
+
+  }
+  return x*hatvany (x,n-1);
+}
+int main()
+{
+
+  int S=1,x=5,n=3;
+  for (int i=1;i<=n;i++){
+    S*=x;
+  }
+cout << S<< " "<<hatvany (x,n)<<endl;
+
+    return 0;
+}
