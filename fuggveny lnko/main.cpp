e0e0ee8351ecde3d9da26173837bfea09e957376
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2022-01-13 09:25:39 +0200

    Add files via upload

---
+++ fuggveny lnko/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int lnko(int a,int b){
+ while (a!=b){
+  if(a>b){
+    a=a-b;
+
+  }else{
+  b=b-a;
+
+  }
+
+  return o;
+
+  }
+  }
+ int main (){
+ int a,b;
+ cout << "a=";
+ cin >>a;
+ cout <<"b=";
+ cin >>b;
+ cout << lnko (a,b);
+
+    return 0;
+}
