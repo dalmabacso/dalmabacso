9cf432f0542eacc068eaf99bbf02afd09a217fc7
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2022-01-13 09:29:26 +0200

    Add files via upload

---
+++ 30.09.21-2.labfeladat/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int v[100][100],n,i,j,S1=0,S2=0;
+    cout << "n=";
+    in>>n;
+    for(int i=0;i<n){
+      for(j=0;j<n;j++){
+
+      }
+    }
+    S1=S1+v[100][100];
+    if(i<j && i+j<n-1){
+
+      }
+    }
+    S2=S2+v[i][j];
+    }
+    cout << S1<< " "<<
+    return 0;
+}
