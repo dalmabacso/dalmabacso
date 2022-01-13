4b1f39569f48b369bd254fba1aa7abd8f2ffd2eb
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2022-01-13 09:15:38 +0200

    Add files via upload

---
+++ string elso(16.11)/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cout <<"Kerem a szoveget";
+    cin >>str;
+    int n=str.size();
+    char pohar =str[n-1];
+    for (int i=n-1;i>0;i--){
+      str[i]=str [i-1];
+    }
+    str[0]=pohar;
+    cout <<str;
+    return 0;
+
+}
