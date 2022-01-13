1bb5f38c706347583fbdb21c94d75e514ef59f53
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2022-01-13 09:14:56 +0200

    Add files via upload

---
+++ string elso(16.11-mas megoldassal)/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cout <<"Kerem a szoveget";
+    cin >>str;
+    int n=str.size();
+    swap (str[0],str[n-1]);
+    for (int i=1;i<n-1;i++){
+        swap (str[i],str[n-1]);
+      str[i]=str [i-1];
+    }
+     cout <<str;
+    return 0;
+
+}
