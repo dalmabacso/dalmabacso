7b8d921eb7b408c90c33d0677c02c2096f1e4ed0
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2022-02-03 07:24:53 +0200

    Add files via upload

---
+++ koch gorbe/main.cpp
@@ -0,0 +1,41 @@
+#include <graphics.h>
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+#define pi 3.14159265359
+using namespace std;
+void Koch(int x1, int y1,float alfa, int xn, int yn,int n){
+int d=sqrt((x1-xn)*(x1-xn)+(y1-yn)*(y1-yn))/3;
+int x2=x1+cos(alfa)*d;
+int y2=y1+sin(alfa)*d;
+int x3=x2+cos(alfa+pi/3)*d;
+int y3=y2+sin(alfa+pi/3)*d;
+int x4=x2+d*cos(alfa);
+int y4=y2+d*sin(alfa);
+int x5=xn;
+int y5=yn;
+if(n==1)
+{
+  line(x1,y1,x2,y2);
+  line(x2,y2,x3,y3);
+  line(x3,y3,x4,y4);
+  line(x4,y4,x5,y5);
+  return;
+}
+n--;
+Koch(x1,y1,alfa,x2,y2,n);
+Koch(x2,y2,alfa+pi/3,x4,y4,n);
+Koch(x3,y3,alfa-pi/3,x4,y4,n);
+Koch(x4,y4,alfa,x5,y5,n);
+}
+int main(){
+int gd,gm;
+gd=gm=0;
+initgraph(&gd,&gm,"");
+setcolor(GREEN);
+setfillstyle(1,WHITE);
+Koch(0,200,0,600,200,4);
+int x;
+cin >>x;
+return 0;
+}
