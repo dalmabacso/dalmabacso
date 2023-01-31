e643360f760c7168544e6756663a4d8e3b5abb5b
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2023-01-31 08:23:14 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
-
+#include <fstream>
+#include<string>
 using namespace std;
 
-int hatvany(int n,int x){
-if (n==1){return x;}
-return x*hatvany(n-1,x);
-
-}
-int main ()
+int main()
 {
-  cout << hatvany (10,2)<<endl;
-  return 0;
+  string szo,szo2;
+  cout <<"kerem a beolvasando szot";
+  getline (cin,szo);
+  int elofordul=0;
+  size_t hely;
+  hely=szo.find("r");
+  if(hely!=string::npos){
+    szo.replace (hely,1,"o");
 
+  }
+  while(hely!string :: npos){
+    hely=szo.find("r",hely+1);
+    if(hely!=string::npos){
+      szo.replace(hely,1,"o");
+
+    }
+  }
+  hely=szo.find("m");
+  if(hely!=string::npos){
+    szo.replace(hely,1,"i");
+  }
+  while(hely!string :: npos){
+      hely=szo,find("m",hely+1);
+       if(hely!=string::npos){
+        szo.replace(hely,1,"i");
+       }
+  }
+  cout <<szo<<endl;
+    return 0;
 }
