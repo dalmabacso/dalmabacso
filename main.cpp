8ce35a9d8b2bf5e42e98089feff666b3adc3c581
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2023-02-02 08:43:48 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,37 +1,26 @@
 #include <iostream>
-#include <fstream>
-#include<string>
-using namespace std;
 
+using namespace std;
+void secventa (int&n){
+int ujszam=0,szorzo=1;
+while(n>0)
+{
+int szj=n%10;
+if(n%100==22){
+  szorzo=szorzo%10;
+
+}else{
+ujszam=szj*szorzo+ujszam;
+szorzo=szorzo*10;
+
+}
+n/=10;
+}
+n=ujszam;
+}
 int main()
 {
-  string szo,szo2;
-  cout <<"kerem a beolvasando szot";
-  getline (cin,szo);
-  int elofordul=0;
-  size_t hely;
-  hely=szo.find("r");
-  if(hely!=string::npos){
-    szo.replace (hely,1,"o");
-
-  }
-  while(hely!string :: npos){
-    hely=szo.find("r",hely+1);
-    if(hely!=string::npos){
-      szo.replace(hely,1,"o");
-
-    }
-  }
-  hely=szo.find("m");
-  if(hely!=string::npos){
-    szo.replace(hely,1,"i");
-  }
-  while(hely!string :: npos){
-      hely=szo,find("m",hely+1);
-       if(hely!=string::npos){
-        szo.replace(hely,1,"i");
-       }
-  }
-  cout <<szo<<endl;
+  int n;
+}
     return 0;
 }
