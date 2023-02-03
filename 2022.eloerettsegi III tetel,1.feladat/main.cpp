d93d0fc75b7835a67ab509c9e1f2e5e92aadc4c7
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2023-02-03 07:46:04 +0200

    Add files via upload

---
+++ 2022.eloerettsegi III tetel,1.feladat/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+void patrate (int n,int &x,int &y){
+for(x=2;x<n/2;x++){
+for(y=x+1;y<n/2;y++){
+    if(x*x*y*y==n)
+    return;
+}
+}
+x=0;y=0;
+return;
+}
+int main()
+{
+
+  int x=1;
+  int y=1;
+  patrate (16.x.y);
+  cout<<x<<" ";
+  cout<<y;
+  return 0;
+}
+
