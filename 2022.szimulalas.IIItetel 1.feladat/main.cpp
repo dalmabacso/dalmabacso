bae9ad04b87e8cc0a1bccfa87d4979f0b021ae2a
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2023-02-09 08:25:30 +0200

    Add files via upload

---
+++ 2022.szimulalas.IIItetel 1.feladat/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+void rest (int x,int y,int n,int &k){
+for(int i=0;i<n;i++){
+  if(i%x==2 && i%y==2){
+    k=i;
+  }
+}
+return;
+}
+int main()
+{
+  int x,y,n,k;
+  cin >>x>>y>>n;
+  rest(x,y,n,k);
+  cout<<k;
+  return 0;
+
+}
