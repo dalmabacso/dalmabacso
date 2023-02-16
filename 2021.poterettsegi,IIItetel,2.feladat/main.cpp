7e969a876f673c85bc1732468063a7e5a94d38de
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2023-02-16 08:07:14 +0200

    Add files via upload

---
+++ 2021.poterettsegi,IIItetel,2.feladat/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k,n,a[i][j];
+    cout<<"k=";
+    cin >>k;
+    cout <<"n=";
+    cin >>n;
+ for(int i=0;i<n;i++){
+  for(int j=0;j<n;j++){
+    cin >> a[i][j];
+  }
+ }
+    for(int i=0;i<n;i++){
+  for(int k=0;k<n;k++){
+    cin >> a[i][j];
+    if(i==j){
+
+    }
+
+
+
+
+
+
+
+    return 0;
+}
