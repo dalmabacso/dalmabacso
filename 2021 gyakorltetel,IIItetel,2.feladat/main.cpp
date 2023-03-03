fcbc491a188934aba827b36b273e57930ff8a85b
dalmabacso <91494118+dalmabacso@users.noreply.github.com>
2023-03-03 07:31:54 +0200

    Add files via upload

---
+++ 2021 gyakorltetel,IIItetel,2.feladat/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int v[100][100];
+    int n;
+    cout<<"n=";
+    cin >>n;
+    for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        if(i+j<n+1){
+          v[i][j]=(i+j+1);
+
+        }else{
+        v[i][j]=n-(i+j+1)+(n);
+
+        }
+
+
+          cout <<v[i][j]<<" ";
+    }
+          cout<<endl;
+       }
+
+return 0;
+}
